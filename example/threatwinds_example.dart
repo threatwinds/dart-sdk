c84ef650329db7553c41695e9ade2f95f0422e60
Osmany Montero <osmontero@icloud.com>
2023-09-06 15:42:19 +0300

    adding waiting notification to example

--- example/threatwinds_example.dart
+++ example/threatwinds_example.dart
@@ -1,13 +1,14 @@
 import 'package:threatwinds/threatwinds.dart';
 
 void main() {
-  var client = Client(hostname: "intelligences.threatwinds.com");
+  var client = Client(hostname: "intelligence.threatwinds.com");
   var request = Request(
       endpoint: "/api/search/v1/entities",
       method: "POST",
       query:
           '{"aggs":{"top-malware-types":{"terms":{"field":"attributes.malware-type.keyword","size":50}},"accuracy-stats":{"stats":{"field":"accuracy"}}},"query":{"filter":[{"term":{"type":{"value":"malware"}}}]},"source":{"excludes":["attributes.malware-family"],"includes":["attributes"]}}');
 
-  final resp = request.doReq<Map<String, dynamic>>(cli: client);
+  var resp = request.doReq<Map<String, dynamic>>(cli: client);
+  print("waiting for values...");
   resp.then((value) => print(value));
 }
