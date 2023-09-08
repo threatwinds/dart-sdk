81bd3d8ea36ef351bece13c75157cf52d77a84c4
Osmany Montero <osmontero@icloud.com>
2023-09-08 12:49:05 +0300

    catch errors

--- lib/src/threatwinds_requests.dart
+++ lib/src/threatwinds_requests.dart
@@ -19,6 +19,16 @@ class Request {
       this.headers});
 
   Future<respObject> doReq<respObject>({required Client cli}) async {
+    const htmlEscapeMode = HtmlEscapeMode(
+      name: 'custom',
+      escapeLtGt: true,
+      escapeQuot: true,
+      escapeApos: true,
+      escapeSlash: true,
+    );
+
+    const HtmlEscape htmlEscape = HtmlEscape(htmlEscapeMode);
+
     http.Response resp;
 
     if (cli.authorization != null) {
@@ -83,9 +93,9 @@ class Request {
     if (resp.statusCode == 200 || resp.statusCode == 202) {
       return jsonDecode(resp.body) as respObject;
     } else if (resp.statusCode == 204) {
-      throw Exception("empty response: ${resp.body}");
+      throw Exception('empty response: ${htmlEscape.convert(resp.body)}');
     } else {
-      throw Exception("unexpected response: ${resp.body}");
+      throw Exception('unexpected response: ${htmlEscape.convert(resp.body)}');
     }
   }
 }
