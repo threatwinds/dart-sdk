e6ac9324202d02734ab4190417ba05308ea3a371
Osmany Montero <osmontero@icloud.com>
2024-04-03 22:24:32 +0300

    adding advanced search

--- lib/src/search_queries.dart
+++ lib/src/search_queries.dart
@@ -33,5 +33,457 @@ class Source {
 
   factory Source.fromJson(Map<String, dynamic> json) => _$SourceFromJson(json);
 
-   Map<String, dynamic> toJson() => _$SourceToJson(this);
+  Map<String, dynamic> toJson() => _$SourceToJson(this);
+}
+
+@JsonSerializable()
+class AdvancedSearch {
+  final Bool? query;
+  final Map<String, Aggs>? aggs;
+  final Collapse? collapse;
+  @JsonKey(name: 'search_after')
+  final List<int>? searchAfter;
+  final Source? source;
+  @JsonKey(name: 'script_fields')
+  final dynamic scriptFields;
+
+  AdvancedSearch({
+    this.query,
+    this.aggs,
+    this.collapse,
+    this.searchAfter,
+    this.source,
+    this.scriptFields,
+  });
+
+  factory AdvancedSearch.fromJson(Map<String, dynamic> json) =>
+      _$AdvancedSearchFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AdvancedSearchToJson(this);
+}
+
+@JsonSerializable()
+class Bool {
+  final List<Query>? filter;
+  final List<Query>? must;
+  @JsonKey(name: 'must_not')
+  final List<Query>? mustNot;
+  final List<Query>? should;
+  @JsonKey(name: 'minimum_should_match')
+  final dynamic minimumShouldMatch;
+
+  Bool({
+    this.filter,
+    this.must,
+    this.mustNot,
+    this.should,
+    this.minimumShouldMatch,
+  });
+
+  factory Bool.fromJson(Map<String, dynamic> json) => _$BoolFromJson(json);
+
+  Map<String, dynamic> toJson() => _$BoolToJson(this);
+}
+
+@JsonSerializable()
+class Query {
+  final Bool? bool;
+
+  Query({
+    this.bool,
+  });
+
+  factory Query.fromJson(Map<String, dynamic> json) => _$QueryFromJson(json);
+
+  Map<String, dynamic> toJson() => _$QueryToJson(this);
+}
+
+@JsonSerializable()
+class Collapse {
+  String? field;
+
+  Collapse({this.field});
+
+  factory Collapse.fromJson(Map<String, dynamic> json) => _$CollapseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CollapseToJson(this);
+}
+
+@JsonSerializable()
+class Aggs {
+  Map<String, Aggs>? aggs;
+  Agg? avg;
+  Agg? sum;
+  Agg? min;
+  Agg? max;
+  Cardinality? cardinality;
+  @JsonKey(name: 'value_count')
+  Agg? valueCount;
+  Agg? stats;
+  @JsonKey(name: 'extended_stats')
+  ExtendedStats? extendedStats;
+  @JsonKey(name: 'matrix_stats')
+  Map<String, List<String>>? matrixStats;
+  Agg? percentiles;
+  @JsonKey(name: 'percentile_ranks')
+  PercentileRanks? percentileRanks;
+  @JsonKey(name: 'top_hits')
+  TopHits? topHits;
+  Terms? terms;
+  @JsonKey(name: 'multi_terms')
+  MultiTerms? multiTerms;
+  Map<String, dynamic>? sampler;
+  @JsonKey(name: 'diversified_sampler')
+  Map<String, dynamic>? diversifiedSampler;
+  @JsonKey(name: 'significant_terms')
+  Agg? significantTerms;
+  @JsonKey(name: 'significant_text')
+  Map<String, dynamic>? significantText;
+  Histogram? histogram;
+  @JsonKey(name: 'date_histogram')
+  Histogram? dateHistogram;
+  Range? range;
+  @JsonKey(name: 'date_range')
+  DateRange? dateRange;
+  @JsonKey(name: 'ip_range')
+  Range? ipRange;
+  Map<String, dynamic>? filter;
+  Map<String, dynamic>? filters;
+  dynamic global;
+  Map<String, String>? nested;
+  @JsonKey(name: 'reverse_nested')
+  dynamic reverseNested;
+  @JsonKey(name: 'sum_bucket')
+  PipelineAgg? sumBucket;
+  @JsonKey(name: 'avg_bucket')
+  PipelineAgg? avgBucket;
+  @JsonKey(name: 'min_bucket')
+  PipelineAgg? minBucket;
+  @JsonKey(name: 'max_bucket')
+  PipelineAgg? maxBucket;
+  @JsonKey(name: 'stats_bucket')
+  PipelineAgg? statsBucket;
+  @JsonKey(name: 'extended_stats_bucket')
+  PipelineAgg? extendedStatsBucket;
+  @JsonKey(name: 'bucket_sort')
+  Map<String, dynamic>? bucketSort;
+  @JsonKey(name: 'cumulative_sum')
+  PipelineAgg? cumulativeSum;
+  PipelineAgg? derivative;
+  @JsonKey(name: 'moving_avg')
+  MovingAvg? movingAvg;
+  @JsonKey(name: 'serial_diff')
+  SerialDiff? serialDiff;
+  @JsonKey(name: 'geo_distance')
+  GeoDistance? geoDistance;
+  @JsonKey(name: 'geohash_grid')
+  Grid? geohashGrid;
+  @JsonKey(name: 'geohex_grid')
+  Grid? geohexGrid;
+  @JsonKey(name: 'geotile_grid')
+  Grid? geotileGrid;
+  @JsonKey(name: 'adjacency_matrix')
+  Map<String, dynamic>? adjacencyMatrix;
+
+  Aggs({
+    this.aggs,
+    this.avg,
+    this.sum,
+    this.min,
+    this.max,
+    this.cardinality,
+    this.valueCount,
+    this.stats,
+    this.extendedStats,
+    this.matrixStats,
+    this.percentiles,
+    this.percentileRanks,
+    this.topHits,
+    this.terms,
+    this.multiTerms,
+    this.sampler,
+    this.diversifiedSampler,
+    this.significantTerms,
+    this.significantText,
+    this.histogram,
+    this.dateHistogram,
+    this.range,
+    this.dateRange,
+    this.ipRange,
+    this.filter,
+    this.filters,
+    this.global,
+    this.nested,
+    this.reverseNested,
+    this.sumBucket,
+    this.avgBucket,
+    this.minBucket,
+    this.maxBucket,
+    this.statsBucket,
+    this.extendedStatsBucket,
+    this.bucketSort,
+    this.cumulativeSum,
+    this.derivative,
+    this.movingAvg,
+    this.serialDiff,
+    this.geoDistance,
+    this.geohashGrid,
+    this.geohexGrid,
+    this.geotileGrid,
+    this.adjacencyMatrix,
+  });
+
+  factory Aggs.fromJson(Map<String, dynamic> json) => _$AggsFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AggsToJson(this);
+}
+
+@JsonSerializable()
+class Grid {
+  String? field;
+  int? precision;
+  dynamic bounds;
+  int? size;
+  int? shardSize;
+
+  Grid({
+    this.field,
+    this.precision,
+    this.bounds,
+    this.size,
+    this.shardSize,
+  });
+
+  factory Grid.fromJson(Map<String, dynamic> json) => _$GridFromJson(json);
+
+  Map<String, dynamic> toJson() => _$GridToJson(this);
+}
+
+@JsonSerializable()
+class GeoDistance {
+  dynamic origin;
+  Range? range;
+
+  GeoDistance({
+    this.origin,
+    this.range,
+  });
+
+  factory GeoDistance.fromJson(Map<String, dynamic> json) =>
+      _$GeoDistanceFromJson(json);
+
+  Map<String, dynamic> toJson() => _$GeoDistanceToJson(this);
+}
+
+@JsonSerializable()
+class SerialDiff {
+  int? lag;
+  @JsonKey(name: 'buckets_path')
+  String? bucketsPath;
+
+  SerialDiff({
+    this.lag,
+    this.bucketsPath,
+  });
+
+  factory SerialDiff.fromJson(Map<String, dynamic> json) =>
+      _$SerialDiffFromJson(json);
+
+  Map<String, dynamic> toJson() => _$SerialDiffToJson(this);
+}
+
+@JsonSerializable()
+class MovingAvg {
+  int? predict;
+  int? window;
+  String? model;
+  Map<String, dynamic>? settings;
+  @JsonKey(name: 'pipeline_agg')
+  PipelineAgg? pipelineAgg;
+
+  MovingAvg({
+    this.predict,
+    this.window,
+    this.model,
+    this.settings,
+    this.pipelineAgg,
+  });
+
+  factory MovingAvg.fromJson(Map<String, dynamic> json) =>
+      _$MovingAvgFromJson(json);
+
+  Map<String, dynamic> toJson() => _$MovingAvgToJson(this);
+}
+
+@JsonSerializable()
+class PipelineAgg {
+  @JsonKey(name: 'buckets_path')
+  String? bucketsPath;
+
+  PipelineAgg({this.bucketsPath});
+
+  factory PipelineAgg.fromJson(Map<String, dynamic> json) =>
+      _$PipelineAggFromJson(json);
+
+  Map<String, dynamic> toJson() => _$PipelineAggToJson(this);
+}
+
+@JsonSerializable()
+class Range {
+  String? field;
+  List<Map<String, dynamic>>? ranges;
+
+  Range({
+    this.field,
+    this.ranges,
+  });
+
+  factory Range.fromJson(Map<String, dynamic> json) => _$RangeFromJson(json);
+
+  Map<String, dynamic> toJson() => _$RangeToJson(this);
+}
+
+@JsonSerializable()
+class DateRange {
+  String? format;
+  String? field;
+  List<Map<String, dynamic>>? ranges;
+
+  DateRange({
+    this.format,
+    this.field,
+    this.ranges,
+  });
+
+  factory DateRange.fromJson(Map<String, dynamic> json) =>
+      _$DateRangeFromJson(json);
+
+  Map<String, dynamic> toJson() => _$DateRangeToJson(this);
+}
+
+@JsonSerializable()
+class Terms {
+  String? field;
+  int? size;
+  String? missing;
+  @JsonKey(name: 'min_doc_count')
+  int? minDocCount;
+
+  Terms({
+    this.field,
+    this.size,
+    this.missing,
+    this.minDocCount,
+  });
+
+  factory Terms.fromJson(Map<String, dynamic> json) => _$TermsFromJson(json);
+
+  Map<String, dynamic> toJson() => _$TermsToJson(this);
+}
+
+@JsonSerializable()
+class Histogram {
+  String? field;
+  dynamic interval;
+
+  Histogram({
+    this.field,
+    this.interval,
+  });
+
+  factory Histogram.fromJson(Map<String, dynamic> json) =>
+      _$HistogramFromJson(json);
+
+  Map<String, dynamic> toJson() => _$HistogramToJson(this);
+}
+
+@JsonSerializable()
+class MultiTerms {
+  List<Agg>? terms;
+  Map<String, String>? order;
+
+  MultiTerms({
+    this.terms,
+    this.order,
+  });
+
+  factory MultiTerms.fromJson(Map<String, dynamic> json) =>
+      _$MultiTermsFromJson(json);
+
+  Map<String, dynamic> toJson() => _$MultiTermsToJson(this);
+}
+
+@JsonSerializable()
+class TopHits {
+  int? size;
+
+  TopHits({this.size});
+
+  factory TopHits.fromJson(Map<String, dynamic> json) => _$TopHitsFromJson(json);
+
+  Map<String, dynamic> toJson() => _$TopHitsToJson(this);
+}
+
+@JsonSerializable()
+class PercentileRanks {
+  String? field;
+  List<int>? values;
+
+  PercentileRanks({
+    this.field,
+    this.values,
+  });
+
+  factory PercentileRanks.fromJson(Map<String, dynamic> json) =>
+      _$PercentileRanksFromJson(json);
+
+  Map<String, dynamic> toJson() => _$PercentileRanksToJson(this);
+}
+
+@JsonSerializable()
+class Agg {
+  String? field;
+  int? size;
+
+  Agg({
+    this.field,
+    this.size,
+  });
+
+  factory Agg.fromJson(Map<String, dynamic> json) => _$AggFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AggToJson(this);
+}
+
+@JsonSerializable()
+class ExtendedStats {
+  String? field;
+  int? sigma;
+
+  ExtendedStats({
+    this.field,
+    this.sigma,
+  });
+
+  factory ExtendedStats.fromJson(Map<String, dynamic> json) =>
+      _$ExtendedStatsFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ExtendedStatsToJson(this);
+}
+
+@JsonSerializable()
+class Cardinality {
+  String? field;
+  @JsonKey(name: 'precision_threshold')
+  int? precisionThreshold;
+
+  Cardinality({
+    this.field,
+    this.precisionThreshold,
+  });
+
+  factory Cardinality.fromJson(Map<String, dynamic> json) =>
+      _$CardinalityFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CardinalityToJson(this);
 }
--- lib/src/search_queries.g.dart
+++ lib/src/search_queries.g.dart
@@ -34,3 +34,433 @@ Map<String, dynamic> _$SourceToJson(Source instance) => <String, dynamic>{
       'includes': instance.includes,
       'excludes': instance.excludes,
     };
+
+AdvancedSearch _$AdvancedSearchFromJson(Map<String, dynamic> json) =>
+    AdvancedSearch(
+      query: json['query'] == null
+          ? null
+          : Bool.fromJson(json['query'] as Map<String, dynamic>),
+      aggs: (json['aggs'] as Map<String, dynamic>?)?.map(
+        (k, e) => MapEntry(k, Aggs.fromJson(e as Map<String, dynamic>)),
+      ),
+      collapse: json['collapse'] == null
+          ? null
+          : Collapse.fromJson(json['collapse'] as Map<String, dynamic>),
+      searchAfter: (json['search_after'] as List<dynamic>?)
+          ?.map((e) => e as int)
+          .toList(),
+      source: json['source'] == null
+          ? null
+          : Source.fromJson(json['source'] as Map<String, dynamic>),
+      scriptFields: json['script_fields'],
+    );
+
+Map<String, dynamic> _$AdvancedSearchToJson(AdvancedSearch instance) =>
+    <String, dynamic>{
+      'query': instance.query,
+      'aggs': instance.aggs,
+      'collapse': instance.collapse,
+      'search_after': instance.searchAfter,
+      'source': instance.source,
+      'script_fields': instance.scriptFields,
+    };
+
+Bool _$BoolFromJson(Map<String, dynamic> json) => Bool(
+      filter: (json['filter'] as List<dynamic>?)
+          ?.map((e) => Query.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      must: (json['must'] as List<dynamic>?)
+          ?.map((e) => Query.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      mustNot: (json['must_not'] as List<dynamic>?)
+          ?.map((e) => Query.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      should: (json['should'] as List<dynamic>?)
+          ?.map((e) => Query.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      minimumShouldMatch: json['minimum_should_match'],
+    );
+
+Map<String, dynamic> _$BoolToJson(Bool instance) => <String, dynamic>{
+      'filter': instance.filter,
+      'must': instance.must,
+      'must_not': instance.mustNot,
+      'should': instance.should,
+      'minimum_should_match': instance.minimumShouldMatch,
+    };
+
+Query _$QueryFromJson(Map<String, dynamic> json) => Query(
+      bool: json['bool'] == null
+          ? null
+          : Bool.fromJson(json['bool'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$QueryToJson(Query instance) => <String, dynamic>{
+      'bool': instance.bool,
+    };
+
+Collapse _$CollapseFromJson(Map<String, dynamic> json) => Collapse(
+      field: json['field'] as String?,
+    );
+
+Map<String, dynamic> _$CollapseToJson(Collapse instance) => <String, dynamic>{
+      'field': instance.field,
+    };
+
+Aggs _$AggsFromJson(Map<String, dynamic> json) => Aggs(
+      aggs: (json['aggs'] as Map<String, dynamic>?)?.map(
+        (k, e) => MapEntry(k, Aggs.fromJson(e as Map<String, dynamic>)),
+      ),
+      avg: json['avg'] == null
+          ? null
+          : Agg.fromJson(json['avg'] as Map<String, dynamic>),
+      sum: json['sum'] == null
+          ? null
+          : Agg.fromJson(json['sum'] as Map<String, dynamic>),
+      min: json['min'] == null
+          ? null
+          : Agg.fromJson(json['min'] as Map<String, dynamic>),
+      max: json['max'] == null
+          ? null
+          : Agg.fromJson(json['max'] as Map<String, dynamic>),
+      cardinality: json['cardinality'] == null
+          ? null
+          : Cardinality.fromJson(json['cardinality'] as Map<String, dynamic>),
+      valueCount: json['value_count'] == null
+          ? null
+          : Agg.fromJson(json['value_count'] as Map<String, dynamic>),
+      stats: json['stats'] == null
+          ? null
+          : Agg.fromJson(json['stats'] as Map<String, dynamic>),
+      extendedStats: json['extended_stats'] == null
+          ? null
+          : ExtendedStats.fromJson(
+              json['extended_stats'] as Map<String, dynamic>),
+      matrixStats: (json['matrix_stats'] as Map<String, dynamic>?)?.map(
+        (k, e) =>
+            MapEntry(k, (e as List<dynamic>).map((e) => e as String).toList()),
+      ),
+      percentiles: json['percentiles'] == null
+          ? null
+          : Agg.fromJson(json['percentiles'] as Map<String, dynamic>),
+      percentileRanks: json['percentile_ranks'] == null
+          ? null
+          : PercentileRanks.fromJson(
+              json['percentile_ranks'] as Map<String, dynamic>),
+      topHits: json['top_hits'] == null
+          ? null
+          : TopHits.fromJson(json['top_hits'] as Map<String, dynamic>),
+      terms: json['terms'] == null
+          ? null
+          : Terms.fromJson(json['terms'] as Map<String, dynamic>),
+      multiTerms: json['multi_terms'] == null
+          ? null
+          : MultiTerms.fromJson(json['multi_terms'] as Map<String, dynamic>),
+      sampler: json['sampler'] as Map<String, dynamic>?,
+      diversifiedSampler: json['diversified_sampler'] as Map<String, dynamic>?,
+      significantTerms: json['significant_terms'] == null
+          ? null
+          : Agg.fromJson(json['significant_terms'] as Map<String, dynamic>),
+      significantText: json['significant_text'] as Map<String, dynamic>?,
+      histogram: json['histogram'] == null
+          ? null
+          : Histogram.fromJson(json['histogram'] as Map<String, dynamic>),
+      dateHistogram: json['date_histogram'] == null
+          ? null
+          : Histogram.fromJson(json['date_histogram'] as Map<String, dynamic>),
+      range: json['range'] == null
+          ? null
+          : Range.fromJson(json['range'] as Map<String, dynamic>),
+      dateRange: json['date_range'] == null
+          ? null
+          : DateRange.fromJson(json['date_range'] as Map<String, dynamic>),
+      ipRange: json['ip_range'] == null
+          ? null
+          : Range.fromJson(json['ip_range'] as Map<String, dynamic>),
+      filter: json['filter'] as Map<String, dynamic>?,
+      filters: json['filters'] as Map<String, dynamic>?,
+      global: json['global'],
+      nested: (json['nested'] as Map<String, dynamic>?)?.map(
+        (k, e) => MapEntry(k, e as String),
+      ),
+      reverseNested: json['reverse_nested'],
+      sumBucket: json['sum_bucket'] == null
+          ? null
+          : PipelineAgg.fromJson(json['sum_bucket'] as Map<String, dynamic>),
+      avgBucket: json['avg_bucket'] == null
+          ? null
+          : PipelineAgg.fromJson(json['avg_bucket'] as Map<String, dynamic>),
+      minBucket: json['min_bucket'] == null
+          ? null
+          : PipelineAgg.fromJson(json['min_bucket'] as Map<String, dynamic>),
+      maxBucket: json['max_bucket'] == null
+          ? null
+          : PipelineAgg.fromJson(json['max_bucket'] as Map<String, dynamic>),
+      statsBucket: json['stats_bucket'] == null
+          ? null
+          : PipelineAgg.fromJson(json['stats_bucket'] as Map<String, dynamic>),
+      extendedStatsBucket: json['extended_stats_bucket'] == null
+          ? null
+          : PipelineAgg.fromJson(
+              json['extended_stats_bucket'] as Map<String, dynamic>),
+      bucketSort: json['bucket_sort'] as Map<String, dynamic>?,
+      cumulativeSum: json['cumulative_sum'] == null
+          ? null
+          : PipelineAgg.fromJson(
+              json['cumulative_sum'] as Map<String, dynamic>),
+      derivative: json['derivative'] == null
+          ? null
+          : PipelineAgg.fromJson(json['derivative'] as Map<String, dynamic>),
+      movingAvg: json['moving_avg'] == null
+          ? null
+          : MovingAvg.fromJson(json['moving_avg'] as Map<String, dynamic>),
+      serialDiff: json['serial_diff'] == null
+          ? null
+          : SerialDiff.fromJson(json['serial_diff'] as Map<String, dynamic>),
+      geoDistance: json['geo_distance'] == null
+          ? null
+          : GeoDistance.fromJson(json['geo_distance'] as Map<String, dynamic>),
+      geohashGrid: json['geohash_grid'] == null
+          ? null
+          : Grid.fromJson(json['geohash_grid'] as Map<String, dynamic>),
+      geohexGrid: json['geohex_grid'] == null
+          ? null
+          : Grid.fromJson(json['geohex_grid'] as Map<String, dynamic>),
+      geotileGrid: json['geotile_grid'] == null
+          ? null
+          : Grid.fromJson(json['geotile_grid'] as Map<String, dynamic>),
+      adjacencyMatrix: json['adjacency_matrix'] as Map<String, dynamic>?,
+    );
+
+Map<String, dynamic> _$AggsToJson(Aggs instance) => <String, dynamic>{
+      'aggs': instance.aggs,
+      'avg': instance.avg,
+      'sum': instance.sum,
+      'min': instance.min,
+      'max': instance.max,
+      'cardinality': instance.cardinality,
+      'value_count': instance.valueCount,
+      'stats': instance.stats,
+      'extended_stats': instance.extendedStats,
+      'matrix_stats': instance.matrixStats,
+      'percentiles': instance.percentiles,
+      'percentile_ranks': instance.percentileRanks,
+      'top_hits': instance.topHits,
+      'terms': instance.terms,
+      'multi_terms': instance.multiTerms,
+      'sampler': instance.sampler,
+      'diversified_sampler': instance.diversifiedSampler,
+      'significant_terms': instance.significantTerms,
+      'significant_text': instance.significantText,
+      'histogram': instance.histogram,
+      'date_histogram': instance.dateHistogram,
+      'range': instance.range,
+      'date_range': instance.dateRange,
+      'ip_range': instance.ipRange,
+      'filter': instance.filter,
+      'filters': instance.filters,
+      'global': instance.global,
+      'nested': instance.nested,
+      'reverse_nested': instance.reverseNested,
+      'sum_bucket': instance.sumBucket,
+      'avg_bucket': instance.avgBucket,
+      'min_bucket': instance.minBucket,
+      'max_bucket': instance.maxBucket,
+      'stats_bucket': instance.statsBucket,
+      'extended_stats_bucket': instance.extendedStatsBucket,
+      'bucket_sort': instance.bucketSort,
+      'cumulative_sum': instance.cumulativeSum,
+      'derivative': instance.derivative,
+      'moving_avg': instance.movingAvg,
+      'serial_diff': instance.serialDiff,
+      'geo_distance': instance.geoDistance,
+      'geohash_grid': instance.geohashGrid,
+      'geohex_grid': instance.geohexGrid,
+      'geotile_grid': instance.geotileGrid,
+      'adjacency_matrix': instance.adjacencyMatrix,
+    };
+
+Grid _$GridFromJson(Map<String, dynamic> json) => Grid(
+      field: json['field'] as String?,
+      precision: json['precision'] as int?,
+      bounds: json['bounds'],
+      size: json['size'] as int?,
+      shardSize: json['shardSize'] as int?,
+    );
+
+Map<String, dynamic> _$GridToJson(Grid instance) => <String, dynamic>{
+      'field': instance.field,
+      'precision': instance.precision,
+      'bounds': instance.bounds,
+      'size': instance.size,
+      'shardSize': instance.shardSize,
+    };
+
+GeoDistance _$GeoDistanceFromJson(Map<String, dynamic> json) => GeoDistance(
+      origin: json['origin'],
+      range: json['range'] == null
+          ? null
+          : Range.fromJson(json['range'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$GeoDistanceToJson(GeoDistance instance) =>
+    <String, dynamic>{
+      'origin': instance.origin,
+      'range': instance.range,
+    };
+
+SerialDiff _$SerialDiffFromJson(Map<String, dynamic> json) => SerialDiff(
+      lag: json['lag'] as int?,
+      bucketsPath: json['buckets_path'] as String?,
+    );
+
+Map<String, dynamic> _$SerialDiffToJson(SerialDiff instance) =>
+    <String, dynamic>{
+      'lag': instance.lag,
+      'buckets_path': instance.bucketsPath,
+    };
+
+MovingAvg _$MovingAvgFromJson(Map<String, dynamic> json) => MovingAvg(
+      predict: json['predict'] as int?,
+      window: json['window'] as int?,
+      model: json['model'] as String?,
+      settings: json['settings'] as Map<String, dynamic>?,
+      pipelineAgg: json['pipeline_agg'] == null
+          ? null
+          : PipelineAgg.fromJson(json['pipeline_agg'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$MovingAvgToJson(MovingAvg instance) => <String, dynamic>{
+      'predict': instance.predict,
+      'window': instance.window,
+      'model': instance.model,
+      'settings': instance.settings,
+      'pipeline_agg': instance.pipelineAgg,
+    };
+
+PipelineAgg _$PipelineAggFromJson(Map<String, dynamic> json) => PipelineAgg(
+      bucketsPath: json['buckets_path'] as String?,
+    );
+
+Map<String, dynamic> _$PipelineAggToJson(PipelineAgg instance) =>
+    <String, dynamic>{
+      'buckets_path': instance.bucketsPath,
+    };
+
+Range _$RangeFromJson(Map<String, dynamic> json) => Range(
+      field: json['field'] as String?,
+      ranges: (json['ranges'] as List<dynamic>?)
+          ?.map((e) => e as Map<String, dynamic>)
+          .toList(),
+    );
+
+Map<String, dynamic> _$RangeToJson(Range instance) => <String, dynamic>{
+      'field': instance.field,
+      'ranges': instance.ranges,
+    };
+
+DateRange _$DateRangeFromJson(Map<String, dynamic> json) => DateRange(
+      format: json['format'] as String?,
+      field: json['field'] as String?,
+      ranges: (json['ranges'] as List<dynamic>?)
+          ?.map((e) => e as Map<String, dynamic>)
+          .toList(),
+    );
+
+Map<String, dynamic> _$DateRangeToJson(DateRange instance) => <String, dynamic>{
+      'format': instance.format,
+      'field': instance.field,
+      'ranges': instance.ranges,
+    };
+
+Terms _$TermsFromJson(Map<String, dynamic> json) => Terms(
+      field: json['field'] as String?,
+      size: json['size'] as int?,
+      missing: json['missing'] as String?,
+      minDocCount: json['min_doc_count'] as int?,
+    );
+
+Map<String, dynamic> _$TermsToJson(Terms instance) => <String, dynamic>{
+      'field': instance.field,
+      'size': instance.size,
+      'missing': instance.missing,
+      'min_doc_count': instance.minDocCount,
+    };
+
+Histogram _$HistogramFromJson(Map<String, dynamic> json) => Histogram(
+      field: json['field'] as String?,
+      interval: json['interval'],
+    );
+
+Map<String, dynamic> _$HistogramToJson(Histogram instance) => <String, dynamic>{
+      'field': instance.field,
+      'interval': instance.interval,
+    };
+
+MultiTerms _$MultiTermsFromJson(Map<String, dynamic> json) => MultiTerms(
+      terms: (json['terms'] as List<dynamic>?)
+          ?.map((e) => Agg.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      order: (json['order'] as Map<String, dynamic>?)?.map(
+        (k, e) => MapEntry(k, e as String),
+      ),
+    );
+
+Map<String, dynamic> _$MultiTermsToJson(MultiTerms instance) =>
+    <String, dynamic>{
+      'terms': instance.terms,
+      'order': instance.order,
+    };
+
+TopHits _$TopHitsFromJson(Map<String, dynamic> json) => TopHits(
+      size: json['size'] as int?,
+    );
+
+Map<String, dynamic> _$TopHitsToJson(TopHits instance) => <String, dynamic>{
+      'size': instance.size,
+    };
+
+PercentileRanks _$PercentileRanksFromJson(Map<String, dynamic> json) =>
+    PercentileRanks(
+      field: json['field'] as String?,
+      values: (json['values'] as List<dynamic>?)?.map((e) => e as int).toList(),
+    );
+
+Map<String, dynamic> _$PercentileRanksToJson(PercentileRanks instance) =>
+    <String, dynamic>{
+      'field': instance.field,
+      'values': instance.values,
+    };
+
+Agg _$AggFromJson(Map<String, dynamic> json) => Agg(
+      field: json['field'] as String?,
+      size: json['size'] as int?,
+    );
+
+Map<String, dynamic> _$AggToJson(Agg instance) => <String, dynamic>{
+      'field': instance.field,
+      'size': instance.size,
+    };
+
+ExtendedStats _$ExtendedStatsFromJson(Map<String, dynamic> json) =>
+    ExtendedStats(
+      field: json['field'] as String?,
+      sigma: json['sigma'] as int?,
+    );
+
+Map<String, dynamic> _$ExtendedStatsToJson(ExtendedStats instance) =>
+    <String, dynamic>{
+      'field': instance.field,
+      'sigma': instance.sigma,
+    };
+
+Cardinality _$CardinalityFromJson(Map<String, dynamic> json) => Cardinality(
+      field: json['field'] as String?,
+      precisionThreshold: json['precision_threshold'] as int?,
+    );
+
+Map<String, dynamic> _$CardinalityToJson(Cardinality instance) =>
+    <String, dynamic>{
+      'field': instance.field,
+      'precision_threshold': instance.precisionThreshold,
+    };
